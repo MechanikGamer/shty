543fd8fd0e2409150d6c21d08891c38e42b3a329
Tomasz Seredynski <123777101+MechanikGamer@users.noreply.github.com>
2025-01-05 02:47:19 +0100

    Add Login Page (#26)
    * Add Login Page
    
    * Change

--- apps/web/app/components/Header/Header.tsx
+++ apps/web/app/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import LoginButton from "./LoginButton";
 import { Logo } from "./Logo";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <div className="flex justify-between items-center px-6 py-6 md:px-14 md:py-14 ">
       <div className="flex items-center">
-        <Logo />
+        <Link href="/">
+          <Logo />
+        </Link>
       </div>
       <div className="flex items-center">
         <LoginButton />
--- apps/web/app/components/Header/LoginButton.tsx
+++ apps/web/app/components/Header/LoginButton.tsx
@@ -1,11 +1,10 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 import colors from "../../lib/colors";
 
 const LoginButton = () => {
   const handleLoginClick = () => {
-    console.log("Login button clicked");
-
     if (typeof window !== "undefined" && window.gtag) {
       window.gtag("event", "login_button_click", {
         event_category: "engagement",
@@ -14,20 +13,23 @@ const LoginButton = () => {
       });
     }
   };
+
   return (
-    <button
-      onClick={handleLoginClick}
-      style={{
-        background: colors.primaryButtonBackground,
-        borderColor: colors.buttonBorder,
-        borderWidth: 1,
-      }}
-      className="flex items-center justify-center rounded-full px-8 py-3 lg:px-12 lg:py-4 border border-solid"
-    >
-      <div className="text-white text-[18px] font-semibold font-['Inter'] leading-[18px]">
-        Login
-      </div>
-    </button>
+    <Link href="/login">
+      <button
+        onClick={handleLoginClick}
+        style={{
+          background: colors.primaryButtonBackground,
+          borderColor: colors.buttonBorder,
+          borderWidth: 1,
+        }}
+        className="flex items-center justify-center rounded-full px-8 py-3 lg:px-12 lg:py-4 border border-solid"
+      >
+        <div className="text-white text-[18px] font-semibold font-['Inter'] leading-[18px]">
+          Login
+        </div>
+      </button>
+    </Link>
   );
 };
 
